445e6e39dd141e9077aad2bb9b9aeb563b508ccb
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-09-30 21:59:58 -0400

    Updated mpdels

--- app/models/Assignment.js
+++ app/models/Assignment.js
@@ -1,47 +1,44 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../../config/database'); 
+const { sequelize } = require('../../config/database');
 
-const Assignment = sequelize.define('Assignment', {
+const Assignment = sequelize.define(
+  'Assignment',
+  {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    points: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 100,
       },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+    },
+    num_of_attempts: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 100,
       },
-      points: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          min: 1,
-          max: 100,
-        },
-      },
-      num_of_attempts: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          min: 1,
-          max: 100,
-        },
-      },
-      deadline: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      assignment_created: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-        allowNull: false,
-      },
-      assignment_updated: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-        allowNull: false,
-      },
-});
+    },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    createdAt: 'assignment_created',
+    updatedAt: 'assignment_updated',
+  }
+);
 
 module.exports = Assignment;
