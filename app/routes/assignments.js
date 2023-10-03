ffd4aebe3961aa621cb43956a32dde15d6d4892e
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-03 18:51:20 -0400

    before testing with integ tests

--- app/routes/assignments.js
+++ app/routes/assignments.js
@@ -5,6 +5,7 @@ const assignmentsController = require("../controllers/AssignmentsController");
 router.get("/", assignmentsController.getHandler);
 router.get("/:id", assignmentsController.getHandler);
 router.put("/:id", assignmentsController.putHandler);
+router.put("/", assignmentsController.putHandler);
 router.post("/", assignmentsController.postHandler);
 router.delete("/:id", assignmentsController.deleteHandler);
 router.use("/", assignmentsController.assignmentsHandler);
