e31cd519726502f4a5c5032ca4d46da2187f6c50
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-02 21:40:47 -0400

    post user credential validations

--- app/controllers/AssignmentsController.js
+++ app/controllers/AssignmentsController.js
@@ -1,54 +1,94 @@
-const assignmentModel = require('../models/assignment');
+const assignmentModel = require("../models/assignment");
+const bcrypt = require("bcrypt");
+const account = require("../models/Account");
 
 async function assignmentsHandler(req, res) {
-    console.log('assignments handler')
+  console.log("assignments handler");
 }
 
 async function getHandler(req, res) {
-    console.log('assignments get handler')
+  console.log("assignments get handler");
 }
 async function putHandler(req, res) {
-    console.log('assignments put handler')
+  console.log("assignments put handler");
 }
 async function postHandler(req, res) {
-    // console.log('assignments post handler')
-    // console.log(req.body);
-    // await assignmentModel.create(req.body);
-    await insertHandler(req, res)
+  accountID = await checkAuth(req.headers.authorization);
+  await insertHandler(req, res, accountID);
 }
 
 async function deleteHandler(req, res) {
-    console.log('assignments delete handler')
+  console.log("assignments delete handler");
 }
 
-
-async function insertHandler(req, res) {
-    const requiredKeys = ['name', 'points', 'num_of_attempts','deadline']; // Replace with your actual required keys
-    const missingKeys = [];
-    requiredKeys.forEach((key) => {
-        if (!(key in req.body)) {
-            missingKeys.push(key);
-        }
+async function checkAuth(authData) {
+  auth = new Buffer(authData.split(" ")[1], "base64").toString().split(":");
+  user = auth[0];
+  pass = auth[1];
+  hashPass = await bcrypt.hash(pass, 10);
+  try {
+    const accountDetails = await account.findOne({
+      where: {
+        email: user,
+      },
     });
 
-    if (missingKeys.length > 0) {
-        return res.status(400).json({ error: `Missing required keys: ${missingKeys.join(', ')}` });
+    if (accountDetails) {
+      const pwMatch = await bcrypt.compare(pass, accountDetails.password);
+      // Account found
+      if (pwMatch) {
+        console.log("Password matched");
+        return accountDetails.id;
+      } else {
+        console.log("Password not matched");
+      }
+    } else {
+      // Account not found
+      console.log("Account not found");
     }
-
-    try {
-        await assignmentModel.create(req.body);
-        return res.status(200).json({ message: 'Assignment created successfully' });
-    } catch (error) {
-        console.error('Error creating assignment:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
+  } catch (error) {
+    console.error("Error finding account:", error);
+  }
 }
 
+async function insertHandler(req, res, accountId) {
+  const requiredKeys = [
+    "name",
+    "points",
+    "num_of_attempts",
+    "deadline",
+    "account_id"
+  ];
+  const missingKeys = [];
+
+  accountData = req.body;
+  accountData.account_id = accountId;
+
+  requiredKeys.forEach((key) => {
+    if (!(key in accountData)) {
+      missingKeys.push(key);
+    }
+  });
+
+  if (missingKeys.length > 0) {
+    return res
+      .status(400)
+      .json({ error: `Missing required keys: ${missingKeys.join(", ")}` });
+  }
+
+  try {
+    await assignmentModel.create(accountData);
+    return res.status(200).json({ message: "Assignment created successfully" });
+  } catch (error) {
+    console.error("Error creating assignment:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
 
 module.exports = {
-    assignmentsHandler: assignmentsHandler,
-    getHandler: getHandler,
-    putHandler: putHandler,
-    postHandler: postHandler,
-    deleteHandler: deleteHandler,
-}
+  assignmentsHandler: assignmentsHandler,
+  getHandler: getHandler,
+  putHandler: putHandler,
+  postHandler: postHandler,
+  deleteHandler: deleteHandler,
+};
