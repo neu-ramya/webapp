8687c980fc4dcfd23f68d353b29c0de2d0d3dae4
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-04 00:18:09 -0400

    changes from debian VM and CI action update

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "assignment_2",
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
+	"assignment_2",
+	process.env.DB_USER,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_HOST,
+		dialect: "mariadb",
+		dialectOptions: {
+			port: 3306,
+			socketPath: "/var/run/mysqld/mysqld.sock"
+		}
+	}
 );
 
 module.exports = {
