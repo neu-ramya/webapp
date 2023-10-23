fe535bc8294c6ea3caf1de39e6c7b5cbc3c64c0f
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-10-22 20:54:50 -0400

    use env for db name

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-	"csye6225",
+	process.env.DB_DATABASE,
 	process.env.DB_USER,
 	process.env.DB_PASS,
 	{
 		host: process.env.DB_HOST,
 		dialect: "mariadb",
 		dialectOptions: {
-			port: 3306,
+			port: process.env.DB_PORT,
 		}
 	}
 );
