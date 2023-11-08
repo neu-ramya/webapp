a9681afb5b6ebab1fd719e463efb6f590489b94b
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-11-07 21:57:10 -0500

    updated logs

--- app/controllers/HealthController.js
+++ app/controllers/HealthController.js
@@ -6,11 +6,11 @@ function validateDBConnection(req, res) {
   sequelize
     .authenticate()
     .then(() => {
-      logger.info("Successfull DB connection")
+      logger.info("Successfull DB connection");
       res.status(200).end();
     })
     .catch((error) => {
-      logger.error("Unable to connect to DB")
+      logger.fatal("Unable to connect to DB");
       res.status(503).end();
     });
 }
@@ -19,12 +19,14 @@ async function healthzHandler(req, res) {
   statdClient.increment('webapp.totalHealthzCounter');
   res.setHeader("Cache-Control", "no-cache");
   if (req.method !== "GET") {
+    logger.warn("Method not allowed");
     res.status(405).end();
     return;
   } else if (
     (req.body && Object.keys(req.body).length > 0) ||
     Object.keys(req.query).length > 0
   ) {
+    logger.warn("Request cannot contain body or query parameters");
     res.status(400).end();
     return;
   } else {
