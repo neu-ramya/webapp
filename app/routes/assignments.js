c8da764e2fdaccb973a60a5a92598f6d3eb9e541
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-11-25 23:46:14 -0500

    inital commit for submission API

--- app/routes/assignments.js
+++ app/routes/assignments.js
@@ -7,9 +7,11 @@ router.get("/:id", assignmentsController.getHandler);
 router.put("/:id", assignmentsController.putHandler);
 router.put("/", assignmentsController.putHandler);
 router.post("/", assignmentsController.postHandler);
+router.post("/:id/submission", assignmentsController.postHandler);
 router.delete("/:id", assignmentsController.deleteHandler);
 router.delete("/", assignmentsController.deleteHandler);
 router.patch("/", assignmentsController.patchHandler);
+router.patch("/:id", assignmentsController.patchHandler);
 router.use("/", assignmentsController.assignmentsHandler);
 
 module.exports = router;
---
+++ app/utils/sendSNSNotification.js
@@ -0,0 +1,25 @@
+const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+
+async function sendNotification(message) {
+  const snsClient = new SNSClient({ region: process.env.AWS_REGION });
+
+  const params = {
+    Message: message,
+    TopicArn: process.env.SNS_TOPIC_ARN,
+  };
+
+  const publishCommand = new PublishCommand(params);
+
+  snsClient
+    .send(publishCommand)
+    .then((data) => {
+      console.log("SNS message published successfully:", data.MessageId);
+    })
+    .catch((err) => {
+      console.error("Error publishing SNS message:", err);
+    });
+}
+
+module.exports = {
+    sendNotification: sendNotification
+}
